ead787ee5fdab0ce0510436aeb29e774600237ee
Rika <lxclxc89816@gmail.com>
2014-08-06 18:57:13 +1200

    Adding post format

---
+++ content.php
@@ -0,0 +1,203 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Akiba
+ * Date: 14-8-6
+ * Time: 下午5:48
+ */
+?>
+<?php if( has_post_format( 'aside' )) { ?>
+    <article id="post-<?php the_ID(); ?>" class="item show-up">
+        <div class="wrap-content format-aside clearfix">
+            <div class="heading">
+                <div class="avatar-icon">
+                    <a href="<?php the_permalink(); ?>" title="永久链接：<?php the_title(); ?>"><?php echo get_avatar( 'lxclxc89816@gmail.com', 46 ); ?></a>
+                    <span class="back"><a href="<?php the_permalink() ?>" title="永久链接：<?php the_title(); ?>">更多</a></span>
+                </div>
+                <div class="post-title">
+                    <div class="title-wrap">
+                        <div class="title">
+                            <a href="<?php the_permalink() ?>" title="永久链接：<?php the_title(); ?>"><?php the_author(); ?> </a>
+                        </div>
+                        <time>
+                            <?php the_time('F j, Y') ?>
+                        </time>
+                    </div>
+                </div>
+                <div class="tag">
+                    <?php the_tags('<ul><li><div class="tag-bar"></div>','</li><li><div class="tag-bar"></div>','</li></ul>'); ?>
+                </div>
+            </div>
+            <div class="post-content">
+                <div class="excerpt">
+                    <?php if(has_excerpt()) : ?>
+                        <?php the_excerpt(); ?>
+                    <?php else : ?>
+                        <?php echo cut_str(strip_tags(apply_filters('the_content',$post->post_content)),180); ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="post-meta">
+                <div class="post-comment">
+                    <?php comments_popup_link( ' 0', ' 1', ' %', 'fa-share'); ?>
+                </div>
+                <div class="post-read">
+                    <a class="read-more fa-plus-square-o" href="<?php the_permalink(); ?>" title="继续阅读：<?php the_title(); ?>"> <span>更多</span></a>
+                </div>
+            </div>
+        </div>
+    </article>
+<?php }elseif( has_post_format( 'link' )) { ?>
+    <article id="post-<?php the_ID(); ?>" class="item show-up">
+        <?php $url = get_post_meta($post->ID,'format-link',true); ?>
+        <div class="wrap-content format-link clearfix">
+            <div class="heading">
+                <div class="avatar-icon">
+                    <a href="<?php the_permalink(); ?>" title="永久链接：<?php the_title(); ?>"><?php echo get_avatar( 'lxclxc89816@gmail.com', 46 ); ?></a>
+                    <span class="back"><a href="<?php the_permalink() ?>" title="永久链接：<?php the_title(); ?>">更多</a></span>
+                </div>
+                <div class="post-title">
+                    <div class="title-wrap">
+                        <div class="title">
+                            <a href="<?php the_permalink() ?>" title="永久链接：<?php the_title(); ?>"><?php the_title(); ?></a>
+                        </div>
+                        <time>
+                            <?php the_time('F j, Y') ?>
+                        </time>
+                    </div>
+                </div>
+                <div class="tag">
+                    <?php the_tags('<ul><li><div class="tag-bar"></div>','</li><li><div class="tag-bar"></div>','</li></ul>'); ?>
+                </div>
+            </div>
+            <div class="post-content">
+                <div class="excerpt">
+                    <?php if(has_excerpt()) : ?>
+                        <?php the_excerpt(); ?>
+                    <?php else : ?>
+                        <?php echo cut_str(strip_tags(apply_filters('the_content',$post->post_content)),180); ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="post-thumb">
+                <?php if ( has_post_thumbnail() ) { ?>
+                    <?php if ( has_post_thumbnail() ){ the_post_thumbnail('home-thumbnail', array('class'=>'thumb')); } ?>
+                <?php } else { ?>
+                    <img src="<?php bloginfo( 'template_url' ); ?>/images/no-picture.jpg" class="thumb"/>
+                <?php } ?>
+            </div>
+            <div class="post-meta">
+                <div class="post-comment">
+                    <?php comments_popup_link( ' 0', ' 1', ' %', 'fa-share'); ?>
+                </div>
+                <div class="post-read">
+                    <a class="read-more fa-plus-square-o" href="<?php the_permalink(); ?>" title="继续阅读：<?php the_title(); ?>"> <span>更多</span></a>
+                </div>
+                <div class="post-link">
+                    <a href="<?php echo $url ?>"class="fa-link"> 外部链接</a>
+                </div>
+            </div>
+        </div>
+    </article>
+<?php }elseif( has_post_format( 'quote' )) { ?>
+    <article id="post-<?php the_ID(); ?>" class="item show-up">
+        <?php $quote = get_post_meta($post->ID,'format-quote',true); ?>
+        <?php $source = get_post_meta($post->ID,'quote-source',true); ?>
+        <div class="wrap-content format-quote clearfix">
+            <div class="heading">
+                <div class="avatar-icon">
+                    <a href="<?php the_permalink(); ?>" title="永久链接：<?php the_title(); ?>"><?php echo get_avatar( 'lxclxc89816@gmail.com', 46 ); ?></a>
+                    <span class="back"><a href="<?php the_permalink() ?>" title="永久链接：<?php the_title(); ?>">更多</a></span>
+                </div>
+                <div class="post-title">
+                    <div class="title-wrap">
+                        <div class="title">
+                            <a href="<?php the_permalink() ?>" title="永久链接：<?php the_title(); ?>"><?php the_title(); ?></a>
+                        </div>
+                        <time>
+                            <?php the_time('F j, Y') ?>
+                        </time>
+                    </div>
+                </div>
+                <div class="tag">
+                    <?php the_tags('<ul><li><div class="tag-bar"></div>','</li><li><div class="tag-bar"></div>','</li></ul>'); ?>
+                </div>
+            </div>
+            <div class="post-content">
+                <div class="excerpt">
+                    <?php if(has_excerpt()) : ?>
+                        <?php the_excerpt(); ?>
+                    <?php else : ?>
+                        <?php echo cut_str(strip_tags(apply_filters('the_content',$post->post_content)),180); ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="post-quote">
+                <p><?php echo $quote ?></p>
+                <span><a href="<?php echo $source ?>">来源</a></span>
+            </div>
+            <div class="post-meta">
+                <div class="post-comment">
+                    <?php comments_popup_link( ' 0', ' 1', ' %', 'fa-share'); ?>
+                </div>
+                <div class="post-read">
+                    <a class="read-more fa-plus-square-o" href="<?php the_permalink(); ?>" title="继续阅读：<?php the_title(); ?>"> <span>更多</span></a>
+                </div>
+                <div class="post-cat">
+                    <i class="fa-folder-open"></i> <?php the_category(' '); ?>
+                </div>
+            </div>
+        </div>
+    </article>
+<?php } else{ ?>
+    <article id="post-<?php the_ID(); ?>" class="item show-up">
+        <div class="wrap-content clearfix">
+            <div class="heading">
+                <div class="avatar-icon">
+                    <a href="<?php the_permalink(); ?>" title="永久链接：<?php the_title(); ?>"><?php echo get_avatar( 'lxclxc89816@gmail.com', 46 ); ?></a>
+                    <span class="back"><a href="<?php the_permalink() ?>" title="永久链接：<?php the_title(); ?>">更多</a></span>
+                </div>
+                <div class="post-title">
+                    <div class="title-wrap">
+                        <div class="title">
+                            <a href="<?php the_permalink() ?>" title="永久链接：<?php the_title(); ?>"><?php the_title(); ?></a>
+                        </div>
+                        <time>
+                            <?php the_time('F j, Y') ?>
+                        </time>
+                    </div>
+                </div>
+                <div class="tag">
+                    <?php the_tags('<ul><li><div class="tag-bar"></div>','</li><li><div class="tag-bar"></div>','</li></ul>'); ?>
+                </div>
+            </div>
+            <div class="post-content">
+                <div class="excerpt">
+                    <?php if(has_excerpt()) : ?>
+                        <?php the_excerpt(); ?>
+                    <?php else : ?>
+                        <?php echo cut_str(strip_tags(apply_filters('the_content',$post->post_content)),180); ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="post-thumb">
+                <?php if ( has_post_thumbnail() ) { ?>
+                    <?php if ( has_post_thumbnail() ){ the_post_thumbnail('home-thumbnail', array('class'=>'thumb')); } ?>
+                <?php } else { ?>
+                    <img src="<?php bloginfo( 'template_url' ); ?>/images/no-picture.jpg" class="thumb"/>
+                <?php } ?>
+            </div>
+            <div class="post-meta">
+                <div class="post-comment">
+                    <?php comments_popup_link( ' 0', ' 1', ' %', 'fa-share'); ?>
+                </div>
+                <div class="post-read">
+                    <a class="read-more fa-plus-square-o" href="<?php the_permalink(); ?>" title="继续阅读：<?php the_title(); ?>"> <span>更多</span></a>
+                </div>
+                <div class="post-cat">
+                    <i class="fa-folder-open"></i> <?php the_category(' '); ?>
+                </div>
+            </div>
+        </div>
+    </article>
+<?php } // 文章样式结束 ?>
--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ add_theme_support( 'menus' );if( function_exists( 'register_nav_menus' ) ) {regi
 add_theme_support( 'post-thumbnails' );
 set_post_thumbnail_size( 430, 9999, true);
 add_image_size( 'home-thumbnail', 430, 9999 ); //430 pixels wide (and unlimited height)
-
+add_theme_support( 'post-formats', array( 'aside', 'quote','link' ) );
 // 中文截断文字
 function cut_str($string, $sublen, $start = 0, $code = 'UTF-8'){if($code == 'UTF-8'){$pa = "/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|\xe0[\xa0-\xbf][\x80-\xbf]|[\xe1-\xef][\x80-\xbf][\x80-\xbf]|\xf0[\x90-\xbf][\x80-\xbf][\x80-\xbf]|[\xf1-\xf7][\x80-\xbf][\x80-\xbf][\x80-\xbf]/";preg_match_all($pa, $string, $t_string);if(count($t_string[0]) - $start > $sublen) return join('', array_slice($t_string[0], $start, $sublen))."...";return join('', array_slice($t_string[0], $start, $sublen));}else{$start = $start*2;$sublen = $sublen*2;$strlen = strlen($string);$tmpstr = '';for($i=0; $i<$strlen; $i++){ if($i>=$start && $i<($start+$sublen)){if(ord(substr($string, $i, 1))>129) $tmpstr.= substr($string, $i, 2);else $tmpstr.= substr($string, $i, 1);}if(ord(substr($string, $i, 1))>129) $i++;}if(strlen($tmpstr)<$strlen ) $tmpstr.= "...";return $tmpstr;}}
 
