2dd675ba4897b54c3208c3bfd63c2ca99ff95963
akibarika <lxclxc89816@gmail.com>
2014-08-12 17:00:38 +1200

    update in work

--- page-works.php
+++ page-works.php
@@ -7,12 +7,14 @@
             <div class="title left"><h2>#MY WORKS</h2></div>
             <div class="work-content right">
                 <div class="work-container">
-                    <div class="content-title"><h2>#illustrations</h2></div>
+                    <div class="content-title"><h2>#website</h2></div>
                     <div class="content-list">
                         <article class="work-here">
                             <div class="wrap">
                                 <div class="work-thumb">
-
+                                    <div class="thumb-image">
+                                        <img src="<?php bloginfo('template_directory'); ?>/images/works/lilim-v1.jpg" >
+                                    </div>
                                 </div>
                                 <div class="work-info">
 
@@ -23,10 +25,23 @@
                 </div>
                 <div class="work-container">
                     <div class="content-title"><h2>#theme</h2></div>
-                    <div class="content-list"></div>
+                    <div class="content-list">
+                        <article class="work-here">
+                            <div class="wrap">
+                                <div class="work-thumb">
+                                    <div class="thumb-image">
+
+                                    </div>
+                                </div>
+                                <div class="work-info">
+
+                                </div>
+                            </div>
+                        </article>
+                    </div>
                 </div>
                 <div class="work-container">
-                    <div class="content-title"><h2>#website</h2></div>
+                    <div class="content-title"><h2>#illustrations</h2></div>
                     <div class="content-list"></div>
                 </div>
             </div>
