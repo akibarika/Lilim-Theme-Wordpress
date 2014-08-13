37f1fa7b3e7119a04e2aabf19b0891893719cb8e
Rika <lxclxc89816@gmail.com>
2014-08-14 02:07:05 +1200

    Update style sheet

--- page-works.php
+++ page-works.php
@@ -9,7 +9,7 @@
                 <div class="work-container">
                     <div class="content-title"><h2>#website</h2></div>
                     <?php
-                        $args = array('post_type'=>'website');
+                        $args = array('post_type'=>'website','posts_per_page' => -1);
                         query_posts($args);
                         if( have_posts() ) :
                     ?>
@@ -19,18 +19,18 @@
                             $link_url = get_post_meta($post->ID,'link',true);
                             $detail = get_post_meta($post->ID,'des',true);
                         ?>
-                        <article class="work-here">
+                        <article id="post-<?php the_ID(); ?>" class="work-here">
                             <div class="wrap">
                                 <div class="work-thumb">
                                     <div class="thumb-image">
                                         <img src="<?php echo $pic_url; ?>">
                                     </div>
                                     <div class="caption">
-                                        <a class="button web" href="<?php echo $link; ?>" target="_blank" ><span>visit</span></a>
+                                        <a class="button web" href="<?php echo $link_url; ?>" target="_blank" ><span>visit</span></a>
                                     </div>
                                 </div>
                                 <div class="work-info">
-                                    <div class="work-title"><a href="<?php echo $link; ?>" target="_blank"><span><?php the_title(); ?></span></a> </div>
+                                    <div class="work-title"><a href="<?php echo $link_url; ?>" target="_blank"><span><?php the_title(); ?></span></a> </div>
                                     <div class="sub"><?php echo $detail; ?></div>
                                 </div>
                             </div>
@@ -42,7 +42,7 @@
                 <div class="work-container">
                     <div class="content-title"><h2>#theme</h2></div>
                         <?php
-                            $args = array('post_type'=>'theme');
+                            $args = array('post_type'=>'theme','posts_per_page' => -1);
                             query_posts($args);
                             if( have_posts() ) :
                         ?>
@@ -52,18 +52,18 @@
                             $link_url = get_post_meta($post->ID,'link',true);
                             $detail = get_post_meta($post->ID,'des',true);
                         ?>
-                        <article class="work-here">
+                        <article id="post-<?php the_ID(); ?>" class="work-here">
                             <div class="wrap">
                                 <div class="work-thumb">
                                     <div class="thumb-image">
                                         <img src="<?php echo $pic_url; ?>" >
                                     </div>
                                     <div class="caption">
-                                        <a class="button theme" href="<?php echo $link; ?>" target="_blank" ><span>private</span></a>
+                                        <a class="button theme" href="<?php echo $link_url; ?>" target="_blank" ><span>private</span></a>
                                     </div>
                                 </div>
                                 <div class="work-info">
-                                    <div class="work-title"><a href="<?php echo $link; ?>" target="_blank"><span><?php the_title(); ?></span></a> </div>
+                                    <div class="work-title"><a href="<?php echo $link_url; ?>" target="_blank"><span><?php the_title(); ?></span></a> </div>
                                     <div class="sub"><?php echo $detail; ?></div>
                                 </div>
                             </div>
@@ -75,7 +75,7 @@
                 <div class="work-container">
                     <div class="content-title"><h2>#illustration</h2></div>
                     <?php
-                        $args = array('post_type'=>'illustration');
+                        $args = array('post_type'=>'illustration','posts_per_page' => -1);
                         query_posts($args);
                         if( have_posts() ) :
                     ?>
@@ -85,18 +85,18 @@
                             $link_url = get_post_meta($post->ID,'link',true);
                             $detail = get_post_meta($post->ID,'des',true);
                         ?>
-                        <article class="work-here">
+                        <article id="post-<?php the_ID(); ?>" class="work-here">
                             <div class="wrap">
                                 <div class="work-thumb">
                                     <div class="thumb-image">
                                         <img src="<?php echo $pic_url; ?>" >
                                     </div>
                                     <div class="caption">
-                                        <a class="button drawing" href="<?php echo $link; ?>" target="_blank" ><span>browse</span></a>
+                                        <a class="button drawing" href="<?php echo $link_url; ?>" target="_blank" ><span>browse</span></a>
                                     </div>
                                 </div>
                                 <div class="work-info">
-                                    <div class="work-title"><a href="<?php echo $link; ?>" target="_blank"><span><?php the_title(); ?></span></a> </div>
+                                    <div class="work-title"><a href="<?php echo $link_url; ?>" target="_blank"><span><?php the_title(); ?></span></a> </div>
                                     <div class="sub"><?php echo $detail; ?></div>
                                 </div>
                             </div>
