80e4a034cdcf03c2233a6c6cfbdfe5a63dd38a4c
AkibaRika <lxclxc89816@gmail.com>
2015-01-11 15:53:16 +1300

    upgraded pixiv page, less and style sheet

--- index.php
+++ index.php
@@ -1,21 +1,32 @@
 <?php get_header(); ?>
-        <div class="container">
-			<section id="post">
-                <div class="pageload-overlay"></div>
+	<div class="container">
+		<section id="post">
+			<div class="pageload-overlay"></div>
+<!--			<div class="coloumn--null">-->
 				<?php if (have_posts()) :
-				while (have_posts()) : 
+				while ( have_posts() ) :
 					the_post();
-                    get_template_part('content');
-				endwhile;else : ?>
+					?>
+
+					<?php
+					get_template_part( 'content' );
+					?>
+
+				<?php
+				endwhile;
+				?>
+<!--			</div>-->
+			<?php
+			else : ?>
 				<article class="item i-404">
 					<div class="wrap-content clearfix">
 						<div class="not-found">
-							<img src="<?php bloginfo('template_directory'); ?>/images/404-not-found.jpg" />
+							<img src="<?php bloginfo( 'template_directory' ); ?>/images/404-not-found.jpg"/>
 						</div>
 					</div>
-				</article> 
-				<?php endif; ?>  
-			</section>
-			<?php wpbeginner_numeric_posts_nav(); ?>
-        </div>
+				</article>
+			<?php endif; ?>
+		</section>
+		<?php wpbeginner_numeric_posts_nav(); ?>
+	</div>
 <?php get_footer(); ?>
