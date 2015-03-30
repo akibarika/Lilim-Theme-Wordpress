8a96a2879af90d652de0dbf973fc111cd972e16e
AkibaRika <lxclxc89816@gmail.com>
2015-03-31 08:43:26 +1300

    updates cookie issue

--- wp-content/themes/Lilim-Theme-Wordpress/tag.php
+++ wp-content/themes/Lilim-Theme-Wordpress/tag.php
@@ -1,19 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Akiba
- * Date: 14-8-2
- * Time: 下午9:28
- * A Tag Template
- */
+
 get_header(); ?>
 	<div class="container">
-		<section id="post" class="<?php echo $_COOKIE['layout'] == 1 ? 'grid--view':'list--view' ?>">
+		<section id="post" class="<?php echo $_COOKIE['layout'] == '1' ? 'grid--view':'list--view' ?>">
 			<div class="pageload-overlay"></div>
-			<div class="column--one column--post show--up <?php echo $_COOKIE['layout'] == 1 ? '':'column--invisible' ?>"></div>
-			<div class="column--two column--post show--up <?php echo $_COOKIE['layout'] == 1 ? '':'column--invisible' ?>"></div>
-			<div class="column--three column--post show--up <?php echo $_COOKIE['layout'] == 1 ? '':'column--invisible' ?>"></div>
-			<div class="column--invisible <?php echo $_COOKIE['layout'] == 1 ? '':'column--show' ?>">
+			<div class="column--one column--post show--up <?php echo $_COOKIE['layout'] == '1' ? '':'column--invisible' ?>"></div>
+			<div class="column--two column--post show--up <?php echo $_COOKIE['layout'] == '1' ? '':'column--invisible' ?>"></div>
+			<div class="column--three column--post show--up <?php echo $_COOKIE['layout'] == '1' ? '':'column--invisible' ?>"></div>
+			<div class="column--invisible <?php echo $_COOKIE['layout'] == '1' ? '':'column--show' ?>">
 				<article id="tag-" class="item show-up kanban list--post">
 					<div class="in-tag">
 						<h2>Tag:<span> <?php single_tag_title(); ?></span></h2>
