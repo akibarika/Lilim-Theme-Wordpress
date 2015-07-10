e48d94e166e5b3b48eba762f6e4701f3d240eac2
AkibaRika <lxclxc89816@gmail.com>
2015-07-11 11:10:13 +1200

    updates share page

--- wp-content/themes/Lilim-Theme-Wordpress/share.php
+++ wp-content/themes/Lilim-Theme-Wordpress/share.php
@@ -1,10 +1,13 @@
+<?php
+	$_description  = get_the_title() . ' - 作者: ' . the_author() . ',首发于' . get_bloginfo('name');
+?>
 <div class="share">
 	<button class="share-toggle-button">
 		<i class="icon-share"></i>
 	</button>
 	<ul class="share-items">
 		<li class="share-item">
-			<a href="http://service.weibo.com/share/share.php?url=<?php the_permalink() ?>&appkey=&title=<?php the_title(); ?>&pic=&ralateUid=&language=zh_cn" onclick="window.open(this.href, 'weibo-share', 'width=550,height=235');return false;" class="share-button">
+			<a href="http://service.weibo.com/share/share.php?url=<?php the_permalink() ?>&appkey=&title=<?php echo $_description; ?>&pic=&ralateUid=&language=zh_cn" onclick="window.open(this.href, 'weibo-share', 'width=550,height=235');return false;" class="share-button">
 				<i class="icon-weibo"></i>
 			</a>
 		</li>
@@ -14,7 +17,7 @@
 			</a>
 		</li>
 		<li class="share-item">
-			<a href="http://twitter.com/share?text=<?php the_title(); ?>&url=<?php the_permalink() ?>" onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;" class="share-button">
+			<a href="http://twitter.com/share?text=<?php echo $_description; ?>&url=<?php the_permalink() ?>" onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;" class="share-button">
 				<i class="icon-twitter"></i>
 			</a>
 		</li>
