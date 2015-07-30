bc1ce5e19b583bbb96340682048a74dee132431b
AkibaRika <lxclxc89816@gmail.com>
2015-07-31 00:54:39 +1200

    added music and its styles

--- wp-content/themes/Lilim-Theme-Wordpress/inc/music.php
+++ wp-content/themes/Lilim-Theme-Wordpress/inc/music.php
@@ -1,2 +1,48 @@
 <?php
 
+function netease_music() {
+	echo netease_music_output();
+}
+
+function netease_music_output() {
+	$max_page = get_netease_max_page();
+	$output   = get_netease_music();
+	$output .= '<div class="music-page-navi">';
+
+	if ( $max_page > 1 ) {
+		$output .= '<a class="nm-loadmore" data-paged="2" data-max="' . $max_page . '" href="javascript:;">加载更多音乐</a>';
+	}
+
+	return $output;
+}
+
+function get_netease_music( $paged = null ) {
+	global $nmjson;
+	$index    = 0;
+	$userid   = nm_get_setting( 'id' ) ? nm_get_setting( 'id' ) : 30829298;
+	$row      = nm_get_setting( 'number' ) ? nm_get_setting( 'number' ) : 4;
+	$contents = $nmjson->netease_user( $userid );
+	array_shift( $contents );
+	$per_page = nm_get_setting( 'perpage' ) ? nm_get_setting( 'perpage' ) : 16;
+	$count    = count( $contents );
+	$max_page = ceil( $count / $per_page );
+	$paged    = $paged ? $paged : 1;
+	$contents = array_slice( $contents, ( ( $paged - 1 ) * $per_page ), $per_page );
+	$is_small = nm_get_setting( 'small' );
+	$size     = 500;
+	$output   = '<div class="album-list">';
+	foreach ( $contents as $content ) {
+		$index ++;
+		$output .= '<div class="album--nice" data-type="163collect" data-thumbnail="' . $content['playlist_coverImgUrl'] . '" data-id="' . $content['playlist_id'] . '"  data-tooltip="' . $content['playlist_name'] . '"><img src="' . $content['playlist_coverImgUrl'] . '?param=' . $size . 'y' . $size . '">
+<i class="fxfont"></i><span class="music-info">' . $content['playlist_name'] . '</span>
+</div>';
+
+		if ( $index % $row == 0 && $index < $per_page ) {
+			$output .= '</div><div class="album-list">';
+		}
+	}
+	$output .= '</div>';
+
+	return $output;
+
+}
--- wp-content/themes/Lilim-Theme-Wordpress/page-music.php
+++ wp-content/themes/Lilim-Theme-Wordpress/page-music.php
@@ -1,12 +1,25 @@
 <?php /* 
 	  	Template Name: Music(音乐电台)
 	  */ ?>
-<?php get_header(); ?>		
-    <div id="page-wrap">
-        <?php while ( have_posts() ) : the_post(); ?>
-        <div class="page-music">
-	        <input class="js-search-input" placeholder="搜索虾米音乐"><div class="js-search-results"></div>
-        </div>
-        <?php endwhile; ?>
-    </div>
+<?php get_header(); ?>
+	<div id="page-wrap">
+		<div class="right-toolbar">
+			<span class="tooltip-left" data-tooltip="分享"><div class="tool-icon icon-share"></div></span>
+			<span class="tooltip-left" data-tooltip="滚动"><div
+					class="tool-icon tool-goto tool-down icon-angle-down"></div></span>
+		</div>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<section id="single" class="music-wrapper page">
+				<article id="netease-music-content">
+					<h1 class="ribbon">
+						<strong class="ribbon-content"><?php echo _e('My Music');?></strong>
+					</h1>
+					<input class="js-search-input" placeholder="搜索虾米音乐">
+					<div class="js-search-results"></div>
+					<?php netease_music(); ?>
+				</article>
+			</section>
+
+		<?php endwhile; ?>
+	</div>
 <?php get_footer(); ?>
